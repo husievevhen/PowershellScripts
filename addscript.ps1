995ec3db6f622ad24389473a09c3a06f4acdac70
Yevhen <33349623+husievevhen@users.noreply.github.com>
2018-05-08 08:52:10 +0300

    Add files via upload

---
+++ addscript.ps1
@@ -0,0 +1,7 @@
+﻿
+$path = "D:\SheluderScripts\ex_vm_v2.ps1"
+$trigger =  New-ScheduledTaskTrigger -Daily -DaysInterval 3 -At 3:48pm
+$User= "Administrator"
+$Action= New-ScheduledTaskAction -Execute "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" -Argument `
+"-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $path"
+Register-ScheduledTask -TaskName "Test2" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
---
+++ ex_vm_v2.ps1
@@ -0,0 +1,26 @@
+﻿[Cmdletbinding()]
+param(
+    [Parameter(Mandatory=$false)]
+        [string] $VmName,
+    [Parameter(Mandatory=$false)]
+        [string] $NetworkPath = "D:\export"
+)
+
+
+
+if($VmName -eq [String]::Empty)
+{
+    [System.Array] $AllVm = Get-VM
+    foreach ($VM in $AllVm)
+    {
+        [String] $VmName = $VM.Name
+        Export -VmName $VmName -NetworkPath $NetworkPath
+ 
+    }
+}
+else
+{
+    Export -VmName $VmName -NetworkPath $NetworkPath
+}
+
+
