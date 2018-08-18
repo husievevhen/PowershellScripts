6528d849692d6087e85e90dc1b503593131924d5
Yevhen <33349623+husievevhen@users.noreply.github.com>
2018-08-18 11:59:28 +0300

    Add files via upload
    First version

---
+++ NoNameParams.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string] $FileName = $args[0] ,          #Имя файла
+    [Parameter(Mandatory=$true)]
+    [string] $CustomError = $args[1],             #Имя ошибки
+    [Parameter(Mandatory=$true)]
+    [Int32] $LinesBefore = $args[2],        #Количество выводимых строк до совпадения
+    [Parameter(Mandatory=$true)]
+    [Int32] $LinesAfter = $args[3]         #Количество выводимых строк после совпадения
+    )
+
+
+    
+Try{
+ Select-String -Path $FileName -Pattern $CustomError -Context $LinesBefore, $LinesAfter 
+}
+Catch{
+    Write-Host "I believe that what doesn’t kill simply makes you stranger...."
+}
