5d1f617f837dccb92e978603b348ad06eaf153a6
Yevhen <33349623+husievevhen@users.noreply.github.com>
2018-08-18 10:35:15 +0300

    Add files via upload

---
+++ FindError.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [Parameter(Mandatory=$false)]
+    [string] $FileName = 'C:\Users\Administrator\Desktop\error.txt' ,          #Имя файла
+    [Parameter(Mandatory=$false)]
+    [string] $CustomError = "Error 44",             #Имя ошибки
+    [Parameter(Mandatory=$false)]
+    [Int32] $LinesBefore = 0,        #Количество выводимых строк до совпадения
+    [Parameter(Mandatory=$false)]
+    [Int32] $LinesAfter = 4         #Количество выводимых строк после совпадения
+    )
+
+
+
+Try{
+
+ Select-String -Path $FileName -Pattern $CustomError -Context $LinesBefore, $LinesAfter
+}
+Catch{
+    Write-Host "I believe that what doesn’t kill simply makes you stranger...."
+}
+
